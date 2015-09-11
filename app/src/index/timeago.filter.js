56fcfe21cf59d900d897e7eb9b3e5bf9e3d22e32
Roi Dayan <roi.dayan@gmail.com>
2015-09-11 11:33:44 +0300

    timeago.filter: add fuzzy month
    Signed-off-by: Roi Dayan <roi.dayan@gmail.com>

--- app/src/index/timeago.filter.js
+++ app/src/index/timeago.filter.js
@@ -14,10 +14,11 @@
         //local: compared to what time? default: now
         //raw: wheter you want in a format of "5 minutes ago", or "5 minutes"
         return function (time, local, raw) {
-            if (!time) return "never";
+            if (!time)
+                return "never";
 
             if (!local) {
-                (local = Date.now())
+                local = Date.now();
             }
 
             if (angular.isDate(time)) {
@@ -28,7 +29,7 @@
 
             if (angular.isDate(local)) {
                 local = local.getTime();
-            }else if (typeof local === "string") {
+            } else if (typeof local === "string") {
                 local = new Date(local).getTime();
             }
 
@@ -43,7 +44,7 @@
                 HOUR = 3600,
                 DAY = 86400,
                 WEEK = 604800,
-                MONTH = 2629744,
+                MONTH = 2592000,
                 YEAR = 31556926,
                 DECADE = 315569260;
 
@@ -51,7 +52,8 @@
             else if (offset < (MINUTE * 60))   span = [ Math.round(Math.abs(offset / MINUTE)), 'min' ];
             else if (offset < (HOUR * 24))     span = [ Math.round(Math.abs(offset / HOUR)), 'hour' ];
             else if (offset < (DAY * 7))       span = [ Math.round(Math.abs(offset / DAY)), 'day' ];
-            else if (offset < (WEEK * 52))     span = [ Math.round(Math.abs(offset / WEEK)), 'week' ];
+            else if (offset < (WEEK * 4))      span = [ Math.round(Math.abs(offset / WEEK)), 'week' ];
+            else if (offset < (MONTH * 12))    span = [ Math.round(Math.abs(offset / MONTH)), 'month' ];
             else if (offset < (YEAR * 10))     span = [ Math.round(Math.abs(offset / YEAR)), 'year' ];
             else if (offset < (DECADE * 100))  span = [ Math.round(Math.abs(offset / DECADE)), 'decade' ];
             else                               span = [ '', 'a long time' ];
@@ -59,9 +61,9 @@
             span[1] += (span[0] === 0 || span[0] > 1) ? 's' : '';
             span = span.join(' ');
 
-            if (raw === true) {
+            if (raw === true)
                 return span;
-            }
+
             return (time <= local) ? span + ' ago' : 'in ' + span;
         }
     }
